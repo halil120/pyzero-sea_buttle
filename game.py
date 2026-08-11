7959675d3e2eba0a07db528d591751cfc3a4c111
halil120 <haliltual@gmail.com>
2026-08-11 19:47:07 +0300

    валидное ко-во кораблей

--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 
 from enum import IntEnum, Enum, auto
-from collections import deque
+from collections import deque,Counter
 
 HEIGHT = 500
 WIDTH = 900
@@ -106,25 +106,83 @@ def get_ship_length(field, start_x, start_y):
                     continue
                 next_x = x + dx
                 next_y = y + dy
+                if not (0 <= next_x < len(field) and 0 <= next_y < len(field[next_x])):
+                    continue
                 if (next_x, next_y) in visited:
                     continue
                 if field[next_x][next_y].is_ship:
                     queue.append((next_x, next_y))
                     visited.add((next_x, next_y))
-    return len(visited)
+    
+    ship_nearby=False
+    for x, y in visited:
+        for dx in (-1, 0, 1):
+            for dy in (-1, 0, 1):
+                if abs(dx) + abs(dy) != 2:
+                    continue
+                next_x = x + dx
+                next_y = y + dy
+                if not (0 <= next_x < len(field) and 0 <= next_y < len(field[next_x])):
+                    continue
+                if (next_x, next_y) not in visited and field[next_x][next_y].is_ship:
+                    ship_nearby=True
+    all_x = {x for x,y in visited}
+    all_y = {y for x,y in visited}
+    is_straight = len(all_x) == 1 or len(all_y) == 1
+    return len(visited),is_straight,ship_nearby
             
 
+def valid_count_of_ship(field):
+    global count_ship_error
+    counter=Counter({1:0,2:0,3:0,4:0})
+    all_visited=set()
+    visited=set()
+    count_ship_error=False
+    eczample=Counter({1:4,2:3,3:2,4:1})
+    for i in range(10):
+        for j in range(10):
+            if not field[i][j].is_ship:
+                continue
+            if (i,j) in visited or (i,j) in all_visited:
+                continue
+            queue = deque([(i, j)])
+            visited = {(i, j)}
+            while queue:
+                x, y = queue.popleft()
+                for dx in (-1, 0, 1):
+                    for dy in (-1, 0, 1):
+                        if abs(dx) + abs(dy) != 1:
+                            continue
+                        next_x = x + dx
+                        next_y = y + dy
+                        if not (0 <= next_x < len(field) and 0 <= next_y < len(field[next_x])):
+                            continue
+                        if (next_x, next_y) in visited:
+                            continue
+                        if field[next_x][next_y].is_ship:
+                            queue.append((next_x, next_y))
+                            visited.add((next_x, next_y))
+                            all_visited.add((next_x, next_y))
+            counter[len(visited)]+=1
+            if counter[1]>eczample[1] or counter[2]>eczample[2] or counter[3]>eczample[3] or counter[4]>eczample[4]:
+                count_ship_error=True
+                return counter
+    return counter
 
 
 
 def valid_ship(field):
     global ship_error
+    ship_error=False
     for row in range(10):
         for col in range(10):
-            if field[row][col].is_ship:
-                len_ship=get_ship_length(field,row,col)
-                if len_ship>4:
-                    ship_error=True
+            if not field[row][col].is_ship:
+                continue
+            len_ship,is_straight,ship_nearby=get_ship_length(field,row,col)
+            if len_ship>4 or ship_nearby or not is_straight:
+                ship_error=True
+                
+                    
     
         
         
@@ -146,16 +204,19 @@ def pixel_to_cordinates(x,y,my=False):
 field=Field_Seeble()
 game_state=StatesOfGame()
 ship_error=False
-
+count_ship_error=False
 field.generate_field_enemy()
 field.generate_field_my()
 
 
 def draw():
+    screen.clear()
     x=field.draw_enemy(screen)
     field.draw_my(screen,x)
     if ship_error:
         screen.draw.text('ship error',(550,200),fontsize=74,color='white')
+    elif count_ship_error:
+        screen.draw.text('count ship error',(450,150),fontsize=74,color='white')
     # screen.blit('sea',(0,0))
 
     
@@ -170,6 +231,7 @@ def on_mouse_up(pos,button):
             clk_x,clk_y=pixel_to_cordinates(x,y,True)
             field.my_field_see[clk_x][clk_y].un_or_plase_ship()
             valid_ship(field.my_field_see)
+            valid_count_of_ship(field.my_field_see)
         
 
 
